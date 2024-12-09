7630933ca31417c0bfe3d4594b96f14205d35206
yusukeh <yusukeh@users.noreply.github.com>
2024-12-09 15:39:24 +0900

    update

--- sql/create_table_wireguard_ipv4.sql
+++ sql/create_table_wireguard_ipv4.sql
@@ -4,5 +4,6 @@ CREATE TABLE wireguard_ipv4(
   id INTEGER PRIMARY KEY,
   email TEXT,
   ipaddr TEXT UNIQUE,
+  delete_flag INTERGER,
   created_at TEXT
 );
--- sql/create_table_wireguard_ipv6.sql
+++ sql/create_table_wireguard_ipv6.sql
@@ -4,5 +4,6 @@ CREATE TABLE wireguard_ipv6(
   id INTEGER PRIMARY KEY,
   email TEXT,
   ipaddr TEXT UNIQUE,
+  delete_flag INTEGER,
   created_at TEXT
 );
